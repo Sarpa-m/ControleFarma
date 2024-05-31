8da06811f4464fb9ae1fd67d5ac1819998156ca6
Mauricio Sarpa <mauricio@sarpa.dev>
2024-05-31 12:30:23 -0300

    atualiza o Método responsável por verificar se o usuário está logado

--- App/Session/Login.php
+++ App/Session/Login.php
@@ -15,7 +15,10 @@ class Login
     {
         if (session_status() != PHP_SESSION_ACTIVE) {
             session_name('controlefarma');
-            session_start();
+            session_start([
+                'cookie_lifetime' => 43200, // Tempo de vida do cookie em segundos (12 horas)
+                //  'read_and_close' => true,   // Fechar a sessão após leitura
+            ]);
         }
     }
 
@@ -38,28 +41,45 @@ class Login
     }
 
     /**
-     * Método responsavel por verificar se o usuário esta logado
-     * @return boolean
+     * Método responsável por verificar se o usuário está logado
+     *
+     * @return boolean Retorna true se o usuário estiver logado, false caso contrário
      */
     public static function isLogged()
     {
-        //INICIA A SESSÃO
+        // INICIA A SESSÃO
         self::init();
 
+        // Verifica se há um ID de usuário na sessão
         if (isset($_SESSION['usuario']['id'])) {
 
-            if ($_SESSION['usuario']['timestamp'] + (3 * 60 * 60) >= time()) {
+            // Verifica se a sessão do usuário ainda é válida (dentro de 10 minutos)
+            if ($_SESSION['usuario']['timestamp'] + (20 * 60) >= time()) {
                 return true;
             }
-            $obUser = User::getUserById($_SESSION['usuario']['id']);
-            if ($obUser instanceof User) {
-                $_SESSION['usuario']['timestamp'] = time();
-                return true;
+
+            // Verifica se a sessão do usuário ainda é válida (dentro de 40 minutos) e atualiza o timestamp
+            if ($_SESSION['usuario']['timestamp'] + (40 * 60) >= time()) {
+                $obUser = User::getUserById($_SESSION['usuario']['id']);
+                if ($obUser instanceof User) {
+                    // Atualiza o timestamp da sessão do usuário
+                    $_SESSION['usuario']['timestamp'] = time();
+                    return true;
+                }
+            }
+
+            // Se a sessão tiver mais que 40 minutos, retorna false
+            if ($_SESSION['usuario']['timestamp'] + (40 * 60) < time()) {
+                self::logout();
+                return false;
             }
         }
+
+        // Se não estiver logado, encerra a sessão do usuário
         self::logout();
         return false;
     }
+
 
     public static function logout()
     {
@@ -68,5 +88,21 @@ class Login
 
         //DESLOGA O USUARIO
         unset($_SESSION['usuario']);
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        // Por último, destrói a sessão
+        session_destroy();
     }
 }
