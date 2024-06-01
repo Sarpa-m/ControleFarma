2362e03681ac3d9a6bf9298939af9400a1248e66
Mauricio Sarpa <mauricio@sarpa.dev>
2024-06-01 12:20:04 -0300

    Corrije os Métodos da View

--- App/Utils/View.php
+++ App/Utils/View.php
@@ -1,69 +1,69 @@
 <?php
-
 namespace App\Utils;
 
 class View
 {
     /**
-     * Variaves padão da view
+     * Variáveis padrão da view
      *
      * @var array
      */
     private static $vars = [];
 
     /**
-     * Metodo responsavel por definir os dados iniciais da classe
+     * Método responsável por definir os dados iniciais da classe
      *
-     * @param  array $var
+     * @param array $vars Array associativo contendo as variáveis iniciais
      */
     public static function init(array $vars = [])
     {
+        // Mescla as variáveis iniciais com as variáveis padrão
         self::$vars = array_merge($vars, self::$vars);
     }
 
     /**
-     * Método responsavel por retornar o conteudo de uma view 
-     * @param  string $view
-     * @return string
+     * Método responsável por retornar o conteúdo de uma view
+     *
+     * @param string $view Nome do arquivo da view
+     * @return string Conteúdo do arquivo da view
+     * @throws \Exception Se o caminho da view for inválido
      */
     private static function getContentView($view)
     {
         $file = __DIR__ . "/../../resources/view/$view.html";
 
+        // Verifica se o arquivo da view existe
         if (file_exists($file)) {
             return file_get_contents($file);
         } else {
-            throw new \Exception("caminho da view invalido <br>resources/view/$view.html", 500);
+            throw new \Exception("Caminho da view inválido: <br>resources/view/$view.html", 500);
         }
     }
 
     /**
      * Método responsável por retornar o conteúdo renderizado de uma view
-     * @param string $view
-     * @param array $var
-     * @return string
+     *
+     * @param string $view Nome do arquivo da view
+     * @param array $vars Variáveis a serem passadas para a view
+     * @return string Conteúdo renderizado da view
      */
     public static function render($view, $vars = [])
     {
-        //conteudo da view
+        // Conteúdo da view
         $contentView = self::getContentView($view);
 
-        //Merge das variaves da view
-        $vars = array_merge($vars, self::$vars);
+        // Mescla as variáveis da view com as variáveis padrão e as passadas como parâmetro
+        $vars = array_merge(self::$vars, $vars);
 
-        //CHAVE DOS ARRAYS DE VARIAVES
+        // Substitui as chaves das variáveis pelos seus valores no conteúdo da view
         $keys = array_keys($vars);
         $keys = array_map(function ($item) {
             return "{{" . $item . "}}";
         }, $keys);
-
         $values = array_values($vars);
-
-
         $contentView = str_replace($keys, $values, $contentView);
 
-
-        //RETORNA O CONTEUDO RENDERIZADO
+        // Retorna o conteúdo renderizado da view
         return str_replace($keys, $values, $contentView);
     }
 }
