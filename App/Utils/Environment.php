4b7bcb6c24f2a769a740a6092a5ceb2153324758
Mauricio Sarpa <mauricio@sarpa.dev>
2024-06-01 13:30:25 -0300

    corrije o Método de Environment

--- App/Utils/Environment.php
+++ App/Utils/Environment.php
@@ -2,24 +2,29 @@
 
 namespace App\Utils;
 
-class Environment{
+class Environment
+{
 
-  /**
-   * Método responsável por carregar as variáveis de ambiente do projeto
-   * @param  string $dir Caminho absoluto da pasta onde encontra-se o arquivo .env
-   */
-  public static function load($dir){
-    //VERIFICA SE O ARQUIVO .ENV EXISTE
-    if(!file_exists($dir.'/.env')){
-      return false;
+    /**
+     * Método responsável por carregar as variáveis de ambiente do projeto
+     * @param  string $dir Caminho absoluto da pasta onde encontra-se o arquivo .env
+     */
+    public static function load($dir)
+    {
+        // VERIFICA SE O ARQUIVO .ENV EXISTE
+        if (!file_exists($dir . '/.env')) {
+            return false;
+        }
+
+        // DEFINE AS VARIÁVEIS DE AMBIENTE
+        $lines = file($dir . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            // Verifica se a linha não está vazia, não é um comentário que começa com #
+            if (!empty($line) && strpos($line, '=') !== false && strpos($line, '#') !== 0) {
+                putenv(trim($line));
+            }
+        }
+
+        return true;
     }
-
-    //DEFINE AS VARIÁVEIS DE AMBIENTE
-    $lines = file($dir.'/.env');
-    foreach($lines as $line){
-      putenv(trim($line));
-     
-    }
-  }
-
 }
